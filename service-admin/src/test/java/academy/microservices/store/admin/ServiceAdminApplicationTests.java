3dc3227de550f342e65050caab4c498623696e95
jarenas98 <jarenas98@outlook.com>
2022-07-23 12:41:18 -0500

    solve dependencies errors

---
+++ service-admin/src/test/java/academy/microservices/store/admin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.microservices.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
